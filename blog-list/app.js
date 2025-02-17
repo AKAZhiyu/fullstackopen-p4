5da27fa94ba1735edfe869758c618b68e76844cc
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-02-17 10:28:00 +0800

    finish exercise 4.16

--- blog-list/app.js
+++ blog-list/app.js
@@ -6,6 +6,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -26,6 +27,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
