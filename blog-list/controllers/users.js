46e84cefafc10b79f0627de87add177b5bc9a11d
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-02-17 15:17:29 +0800

    finish exercise 4.23

--- blog-list/controllers/users.js
+++ blog-list/controllers/users.js
@@ -5,18 +5,12 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
     const { username, name, password } = request.body
 
-    if (!password) {
-        return response.status(400).json({ error: 'Password is required' })
-    }
+    if (!password || password.length < 3) {
+        return response.status(400).json({ error: 'password missing or too short' })
+      }
 
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
-
-    
-
-    if (password.length < 3) {
-        return response.status(400).json({ error: 'Password must be at least 3 characters long' })
-    }
 
     const user = new User({
         username,
