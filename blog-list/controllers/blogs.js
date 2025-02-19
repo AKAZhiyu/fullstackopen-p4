3f44d04601e1f91e2bf18167259e9ee2b3411c6f
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-02-19 14:52:03 +0800

    fix bug: create blog return only userid

--- blog-list/controllers/blogs.js
+++ blog-list/controllers/blogs.js
@@ -33,6 +33,7 @@ blogsRouter.post('/', async (request, response) => {
     })
 
     const savedBlog = await blog.save()
+    await savedBlog.populate('user', { username: 1, name: 1, id: 1 })
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
     response.status(201).json(savedBlog)
