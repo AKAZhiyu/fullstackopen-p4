9b6b5b36f5b29da759df1832d455022c385a4064
AKAZhiyu <zhixuan.z2003@gmail.com>
2025-02-19 16:55:12 +0800

    fix return error

--- blog-list/controllers/blogs.js
+++ blog-list/controllers/blogs.js
@@ -36,7 +36,7 @@ blogsRouter.post('/', async (request, response) => {
     await savedBlog.populate('user', { username: 1, name: 1, id: 1 })
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
-    response.status(201).json(savedBlog)
+    return response.status(201).json(savedBlog)
 
 })
 
